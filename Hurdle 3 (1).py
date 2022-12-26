6aecdb0fa0ba4e22e97ee245fd8b5028bdd79024
Priyanandepu <89314379+Priyanandepu@users.noreply.github.com>
2022-12-26 12:13:15 +0530

    Add files via upload

---
+++ Hurdle 3 (1).py
@@ -0,0 +1,26 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+while at_goal()==False:
+    if front_is_clear()==True:
+        move()
+    elif wall_in_front()==True:
+        jump()
+    
+    
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
